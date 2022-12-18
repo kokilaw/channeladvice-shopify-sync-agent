25aa6e686475c2f67ddb32e42486f311d1e0dcf3
kokilaw <kokilapc@gmail.com>
2022-12-19 02:13:27 +0530

    feature(): logs updated

--- src/clients/shopify-admin-api/ShopifyAdminAPIClient.js
+++ src/clients/shopify-admin-api/ShopifyAdminAPIClient.js
@@ -2,8 +2,10 @@ const axiosHttpClient = require('./AxiosHttpClient')
 
 
 const createOrder = async (payload) => {
+    console.log(`[ShopifyAdminAPIClient] createOrder - payload:[${JSON.stringify(payload)}]`)
     try {
         const { data } = await axiosHttpClient.post('/orders.json', { order: payload });
+        console.log(`[ShopifyAdminAPIClient] createOrder - response:[${JSON.stringify(data)}]`)
         return data.order;
     } catch (e) {
         console.log(JSON.stringify(e.response.data.errors));
@@ -12,8 +14,10 @@ const createOrder = async (payload) => {
 }
 
 const createPaymentForOrder = async (orderId, payload) => {
+    console.log(`[ShopifyAdminAPIClient] createPaymentForOrder - orderId[${orderId}] payload:[${JSON.stringify(payload)}]`)
     try {
         const { data } = await axiosHttpClient.post(`/orders/${orderId}/transactions.json`, { transaction: payload });
+        console.log(`[ShopifyAdminAPIClient] createPaymentForOrder - response:[${JSON.stringify(data)}]`)
         return data;
     } catch (e) {
         console.log(JSON.stringify(e.response.data.errors));
